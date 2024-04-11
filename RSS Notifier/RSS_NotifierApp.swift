bace6681a511364cac1528d9b1c4e2215a01108e
David DeGraw <degraw@fastmail.com>
2024-04-10 21:03:05 -0600

    Initial Commit

---
+++ RSS Notifier/RSS_NotifierApp.swift
@@ -0,0 +1,17 @@
+//
+//  RSS_NotifierApp.swift
+//  RSS Notifier
+//
+//  Created by David DeGraw on 4/10/24.
+//
+
+import SwiftUI
+
+@main
+struct RSS_NotifierApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
